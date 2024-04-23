7d0acfc2264ecd07c2245e6fa21ad4bf642d264e
ProvidedPlay <mosesreid@hotmail.com>
2024-04-23 16:22:14 -0400

    Set up the Featured Projects page and it's Card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,26 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
+const Card = ({ title, description, imageSrc, url }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return(
-    null
+    <VStack bg="white" borderRadius="3%" overflow="hidden">
+      <VStack spacing={4}>
+        <Image src={imageSrc} alt="Project Image" width="100%"/>
+      </VStack>
+      <VStack color="black" p={4} alignItems="left">
+        <Heading size="md">{title}</Heading>
+        <Text color="dimgray">{description}</Text>
+        <HStack>
+          <a href={url}> See more</a>
+          <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 };
 
