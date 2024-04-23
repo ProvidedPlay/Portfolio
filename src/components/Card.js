c63d9f45f44b84f2f013f13d1c8ce2a3005b9157
ProvidedPlay <mosesreid@hotmail.com>
2024-04-23 16:38:30 -0400

    Set up the Featured Projects page and it's Card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,9 @@ const Card = ({ title, description, imageSrc, url }) => {
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return(
-    <VStack bg="white" borderRadius="3%" overflow="hidden">
+    <VStack bg="white" rounded="2xl" overflow="hidden">
       <VStack spacing={4}>
-        <Image src={imageSrc} alt="Project Image" width="100%"/>
+        <Image src={imageSrc} alt="Project Image" width="100%" rounded="2xl"/>
       </VStack>
       <VStack color="black" p={4} alignItems="left">
         <Heading size="md">{title}</Heading>
