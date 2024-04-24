214cee36af5d984aba1ee8b52a358f7d0e3cd75f
ProvidedPlay <mosesreid@hotmail.com>
2024-04-24 17:21:59 -0400

    updated footer with my name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Pete • © 2022</p>
+          <p>Moses • © 2024</p>
         </Flex>
       </footer>
     </Box>
